a66a3f49c7a99caf0f288c2bd22fe32cf8479d6e
SHoang6702 <shoang6702@gmail.com>
2023-10-31 02:52:27 -0500

    working read me, missing video

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # LogoLord
+
+The program allows you to generated SVG images using a command line prompt
+
+## Installation
+
+Download via github by cloning the repository, npm install in the command line
+
+## Usage
+
+Type node index.js in the command line
+
+Github Repository: 
+
+https://github.com/SHoang6702/LogoLord
+
+Youtube:
+
+
+![Questions](./assets/images/questions.png)
+
+## Credits
+Collaborators:
+Sebastian Hoang
+## Features
+
+## License
+None
--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function writeToFile(fileName, data) {
         if (err) { 
             return console.log(err);
         }
-        console.log("logo.svg: Generated successfully")
+        console.log("Generated logo.svg")
     })
 }
 async function init() {
@@ -94,6 +94,28 @@ async function init() {
     writeToFile(svgFile, svgString)
 }
 init()
+
+//!! Below is mew checklisting the acceptance critera!!
+// GIVEN a command-line application that accepts user input
+
+// WHEN I am prompted for text
+// THEN I can enter up to three characters (completed)
+
+// WHEN I am prompted for the text color
+// THEN I can enter a color keyword (OR a hexadecimal number) (Compelted)
+
+// WHEN I am prompted for a shape
+// THEN I am presented with a list of shapes to choose from: circle, triangle, and square (Completed)
+
+// WHEN I am prompted for the shape's color
+// THEN I can enter a color keyword (OR a hexadecimal number) (Completed)
+
+// WHEN I have entered input for all the prompts
+// THEN an SVG file is created named `logo.svg`
+// AND the output text "Generated logo.svg" is printed in the command line (Completed)
+
+// WHEN I open the `logo.svg` file in a browser
+// THEN I am shown a 300x200 pixel image that matches the criteria I entered (Completed)
 
 
 //!!!Below is a failed experimental version, keeping to work on in future, but will not be used for now!!!
